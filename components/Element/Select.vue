ec3b272e7462cae54b4591d22d34e529636fcaa0
Yilee <yileefrontend@gmail.com>
2024-10-04 23:53:06 +0800

    [ui]select

---
+++ components/Element/Select.vue
@@ -0,0 +1,14 @@
+<script setup>
+const model = defineModel({
+    default: null
+});
+</script>
+
+<template>
+    <select
+        v-model="model"
+        class="outline-none border border-solid border-slate-200 focus:border-primary-dark duration-100 disabled:cursor-not-allowed"
+    >
+        <slot></slot>
+    </select>
+</template>
