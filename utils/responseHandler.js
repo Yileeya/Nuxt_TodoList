5128d9b3effec508f073cf9a5424753a7bb70bc2
Yilee <yileefrontend@gmail.com>
2024-10-05 16:10:36 +0800

    [utils]api response處理調整_useFetch改成$fetch結構

--- utils/responseHandler.js
+++ utils/responseHandler.js
@@ -1,23 +1,5 @@
-export const handleResponse = (data, error) => {
-    let success = false;
-    let msg = '';
-    if (data.value) {
-        const { statusCode, message } = data.value;
-        if (statusCode === 200) {
-            useNuxtApp().$toast.success(message);
-            success = true;
-            msg = message;
-        } else {
-            useNuxtApp().$toast.error('出現錯誤，請重新嘗試。');
-            msg = '出現錯誤，請重新嘗試。';
-        }
-    } else if (error.value) {
-        const errorMessages = error.value.data.message || ['出現錯誤，請重新嘗試。'];
-        useNuxtApp().$toast.error(errorMessages.join('，'));
-        msg = errorMessages.join('，');
-    }
-    return {
-        success: success,
-        message: msg
-    };
+export const handleErrorResponse = error => {
+    const errData = error.response._data;
+    const errorMessages = errData.message || ['出現錯誤，請重新嘗試。'];
+    useNuxtApp().$toast.error(errorMessages.join('，'));
 };
