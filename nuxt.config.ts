7d25b6e57cdd6669759dbf1f34a33b4bd7511632
Yilee <yileefrontend@gmail.com>
2024-10-05 19:39:46 +0800

    render部署設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    compatibilityDate: "2024-04-03",
-    devtools: {enabled: true},
-    css: ["~/assets/css/main.css"],
+    compatibilityDate: '2024-04-03',
+    devtools: { enabled: true },
+    css: ['~/assets/css/main.css'],
     postcss: {
         plugins: {
             tailwindcss: {},
-            autoprefixer: {},
+            autoprefixer: {}
         }
     },
     nitro: {
-        plugins: ["@/server/index"],
-    }
+        plugins: ['@/server/index']
+    },
+    server: {
+        host: '0.0.0.0'
+    },
+    target: 'server'
 });
