0dcd2a4c479851982abff0c5428ac9e338166b5c
Yilee <yileefrontend@gmail.com>
2024-10-05 18:36:29 +0800

    [環境]設定head、SeoMeta、favicon

--- app.vue
+++ app.vue
@@ -1,3 +1,17 @@
+<script setup>
+useHead({
+    title: 'Nuxt - TodoList',
+    meta: [{ name: 'description', content: 'use mongoose and nuxt.' }]
+});
+useSeoMeta({
+    title: 'Nuxt - TodoList',
+    ogTitle: 'Nuxt - TodoList',
+    description: 'use mongoose and nuxt.',
+    ogDescription: 'use mongoose and nuxt.',
+    ogImage: 'https://images.unsplash.com/photo-1650735310415-392ab5378954',
+    twitterCard: 'https://images.unsplash.com/photo-1650735310415-392ab5378954'
+});
+</script>
 <template>
     <div class="app-bg-gradient p-2 md:p-10 min-h-dvh flex items-center justify-center">
         <NuxtPage />
